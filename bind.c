72d0f390dd77ccee04b9541dd0df964f7e09554f
Moritz Barsnick <barsnick@gmx.net>
2022-07-28 16:10:08 +0200

    add various binding options
    This includes binding to a certain source port, excluding source ports, and
    binding the source address only for certain socket types.
    
    This removes the original inline comment (as already committed as
    README.md).
    
    Taken from here:
    https://gist.github.com/zohead/9950663ca01952c940eb

--- bind.c
+++ bind.c
@@ -1,6 +1,8 @@
 /*
    Copyright (C) 2000  Daniel Ryde
 
+   Modified by Uranus Zhou (2014)
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
@@ -11,42 +13,6 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 */
-
-/*
-   LD_PRELOAD library to make bind and connect to use a virtual
-   IP address as localaddress. Specified via the enviroment
-   variable BIND_ADDR.
-
-   Compile on Linux with:
-   gcc -nostartfiles -fpic -shared bind.c -o bind.so -ldl -D_GNU_SOURCE
-
-
-   Example in bash to make inetd only listen to the localhost
-   lo interface, thus disabling remote connections and only
-   enable to/from localhost:
-
-   BIND_ADDR="127.0.0.1" LD_PRELOAD=./bind.so /sbin/inetd
-
-
-   Example in bash to use your virtual IP as your outgoing
-   sourceaddress for ircII:
-
-   BIND_ADDR="your-virt-ip" LD_PRELOAD=./bind.so ircII
-
-   Note that you have to set up your servers virtual IP first.
-
-
-   This program was made by Daniel Ryde
-   email: daniel@ryde.net
-   web:   http://www.ryde.net/
-
-   TODO: I would like to extend it to the accept calls too, like a
-   general tcp-wrapper. Also like an junkbuster for web-banners.
-   For libc5 you need to replace socklen_t with int.
-*/
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -54,14 +20,30 @@
 #include <netinet/in.h>
 #include <dlfcn.h>
 #include <errno.h>
+#include <string.h>
 
 int (*real_bind)(int, const struct sockaddr *, socklen_t);
 int (*real_connect)(int, const struct sockaddr *, socklen_t);
 
-char *bind_addr_env;
+char *bind_addr_env, *connect_bind_addr_env, *bind_port_env, *bind_type_env, *exclude_ports_env, *connect_port_env, *connect_type_env, *connect_bind_port_env;
 unsigned long int bind_addr_saddr;
-unsigned long int inaddr_any_saddr;
 struct sockaddr_in local_sockaddr_in[] = { 0 };
+int bind_port_ns = -1, connect_port_ns = -1;
+int bind_type = -1, connect_type = -1;
+
+static int get_sock_type(const char *str)
+{
+	if (strcasecmp(str, "TCP") == 0)
+		return SOCK_STREAM;
+	else if (strcasecmp(str, "UDP") == 0)
+		return SOCK_DGRAM;
+	else if (strcasecmp(str, "RAW") == 0)
+		return SOCK_RAW;
+	else if (strcasecmp(str, "PACKET") == 0)
+		return SOCK_PACKET;
+	else
+		return -1;
+}
 
 void _init (void)
 {
@@ -77,13 +59,51 @@ void _init (void)
 		fprintf (stderr, "dlsym (connect): %s\n", err);
 	}
 
-	inaddr_any_saddr = htonl (INADDR_ANY);
-	if (bind_addr_env = getenv ("BIND_ADDR")) {
+	if (bind_addr_env = getenv ("BIND_ADDR"))
 		bind_addr_saddr = inet_addr (bind_addr_env);
+
+	if (bind_port_env = getenv ("BIND_PORT"))
+		bind_port_ns = htons (atoi(bind_port_env));
+
+	if (bind_type_env = getenv ("BIND_TYPE"))
+		bind_type = get_sock_type(bind_type_env);
+
+	exclude_ports_env = getenv ("EXCLUDE_PORTS");
+
+	if (connect_port_env = getenv ("CONNECT_PORT"))
+		connect_port_ns = htons (atoi(connect_port_env));
+
+	if (connect_type_env = getenv ("CONNECT_TYPE"))
+		connect_type = get_sock_type(connect_type_env);
+
+	if (connect_bind_addr_env = getenv ("CONNECT_BIND_ADDR")) {
 		local_sockaddr_in->sin_family = AF_INET;
-		local_sockaddr_in->sin_addr.s_addr = bind_addr_saddr;
+		local_sockaddr_in->sin_addr.s_addr = inet_addr (connect_bind_addr_env);
 		local_sockaddr_in->sin_port = htons (0);
 	}
+
+	if (connect_bind_port_env = getenv ("CONNECT_BIND_PORT"))
+		local_sockaddr_in->sin_port = htons (atoi(connect_bind_port_env));
+}
+
+static int check_port(int port)
+{
+	char *tmp = exclude_ports_env, *str = NULL;
+	if (tmp == NULL) return 0;
+
+	while (1) {
+		char szPort[50] = {0};
+		str = strchr(tmp, ',');
+		if (str == NULL)
+			strncpy(szPort, tmp, sizeof(szPort));
+		else
+			strncpy(szPort, tmp, str - tmp);
+		if (atoi(szPort) == port) return 1;
+		if (str == NULL) break;
+		tmp = str + 1;
+	}
+
+	return 0;
 }
 
 int bind (int fd, const struct sockaddr *sk, socklen_t sl)
@@ -91,12 +111,20 @@ int bind (int fd, const struct sockaddr *sk, socklen_t sl)
 	static struct sockaddr_in *lsk_in;
 
 	lsk_in = (struct sockaddr_in *)sk;
-/*	printf("bind: %d %s:%d\n", fd, inet_ntoa (lsk_in->sin_addr.s_addr),
-		ntohs (lsk_in->sin_port));*/
-        if ((lsk_in->sin_family == AF_INET)
-		&& (lsk_in->sin_addr.s_addr == inaddr_any_saddr)
-		&& (bind_addr_env)) {
-		lsk_in->sin_addr.s_addr = bind_addr_saddr;
+	if (lsk_in->sin_family == AF_INET || lsk_in->sin_family == AF_INET6) {
+		int type, length = sizeof(int);
+
+		getsockopt(fd, SOL_SOCKET, SO_TYPE, &type, &length);
+
+		if (check_port(ntohs(lsk_in->sin_port)) == 0 && (bind_type_env == NULL || bind_type == type)) {
+			// change bind address
+			if (lsk_in->sin_addr.s_addr == htonl (INADDR_ANY) && bind_addr_env)
+				lsk_in->sin_addr.s_addr = bind_addr_saddr;
+
+			// change bind port
+			if (bind_port_env)
+				lsk_in->sin_port = bind_port_ns;
+		}
 	}
 	return real_bind (fd, sk, sl);
 }
@@ -104,13 +132,18 @@ int bind (int fd, const struct sockaddr *sk, socklen_t sl)
 int connect (int fd, const struct sockaddr *sk, socklen_t sl)
 {
 	static struct sockaddr_in *rsk_in;
-	
+
 	rsk_in = (struct sockaddr_in *)sk;
-/*	printf("connect: %d %s:%d\n", fd, inet_ntoa (rsk_in->sin_addr.s_addr),
-		ntohs (rsk_in->sin_port));*/
-        if ((rsk_in->sin_family == AF_INET)
-		&& (bind_addr_env)) {
-		real_bind (fd, (struct sockaddr *)local_sockaddr_in, sizeof (struct sockaddr));
+	if (rsk_in->sin_family == AF_INET || rsk_in->sin_family == AF_INET6) {
+		int type, length = sizeof(int);
+
+		getsockopt(fd, SOL_SOCKET, SO_TYPE, &type, &length);
+
+		if ((connect_port_env == NULL || connect_port_ns == rsk_in->sin_port) && (connect_type_env == NULL || connect_type == type)) {
+			// change connect bind address or bind port
+			if (connect_bind_addr_env)
+				real_bind (fd, (struct sockaddr *)local_sockaddr_in, sizeof (struct sockaddr));
+		}
 	}
 	return real_connect (fd, sk, sl);
 }
